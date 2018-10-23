d9eccb3e81dc3e02770f6144c44f31ebaa8ed224
Fred Lee <Fred Lee@LAPTOP-LB7RQ956>
2018-10-23 15:29:01 +0900

    until "get" method

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,9 +30,30 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			if (line == null) return;
+			log.debug("request line : {}", line);
+
+			String url = HttpRequestUtils.getUrl(line);
+			if (url.startsWith("/create")) {
+				int index = url.indexOf("?");
+				String queryString = url.substring(index + 1);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+				User user = new User(params.get("userid"), params.get("password"), params.get("name"), params.get("email"));
+				log.debug("User : {}", user);
+				
+				url = "/index.html";
+			}
+
+//			while (!"".equals(line)) {
+//				line = br.readLine();
+//				log.debug("header : {}", line);
+//			}
+
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+//			byte[] body = "Hello World".getBytes();
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
